85b4c813405f75ea2f2f67c66ce2431677411984
jfsvak <jfsvak@gmail.com>
2015-06-18 15:34:49 +0800

    Added new tests and fill basedata json file

---
+++ RuleEngineMain/src/test/testRuleEngineInitialiser.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+
+#include "ruleenginetestutils.h"
+
+#include "../ruleengine/RuleEngine_sbx.h"
+
+using namespace std;
+using namespace sbx;
+
+class RuleEngineInitialiser : public ::testing::Test  {
+protected:
+    void SetUp() {
+    	RuleEngine::_printDebugAtValidation = false;
+    	re = RuleEngine();
+
+        re.initConstants(get_file_contents("basedata-all.json"));
+        re.initKoncepts(get_file_contents("basedata-all.json"));
+        re.initUnionAgreements(get_file_contents("basedata-all.json"));
+        re.parseRuleCatalogueJSON(get_file_contents("rule-catalogue.json"));
+    }
+
+    RuleEngine re;
+};
+
+
+
+
+
+
+
---
+++ RuleEngineMain/src/test/testUnionAgreementParsing.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+
+#include "../ruleengine/KonceptInfo_sbx.h"
+
+#include "testRuleEngineInitialiser.cpp"
+
+using namespace std;
+using namespace sbx;
+
+class UnionAgreementParsing : public RuleEngineInitialiser {
+protected:
+    virtual void SetUp() {
+    	RuleEngineInitialiser::SetUp();
+
+        KonceptInfo ki {PROSA, 5, 0, // UnderkonceptOid:OSV 25-49
+        	{ {11, "true"}, // Parameter-Basis
+        	  {1, "true"}, // Solidarisk faellestarif
+			  {15, "true"}, // FG span
+			  {6, "true"} // SEB Firmapensionspulje
+        	} };
+        re.initContext(ki, OUTSIDE);
+    }
+};
+
+TEST_F(UnionAgreementParsing, Parsing_UnionAgreements) {
+	RuleEngine::_printDebug = true;
+	RuleEngine::_printDebugAtValidation = true;
+
+	re.getContainer().printUnionAgreements();
+}
+
+
+
+
+
+
